9d067e2b443103be99cee3d9790d5f40f97779bc
Ingo Breßler <dev@ingobressler.net>
2026-04-01 23:21:31 +0200

    style(DobotDriver): consistent formatting and minor spelling corrections

--- dobot/DobotDriver.py
+++ dobot/DobotDriver.py
@@ -2,10 +2,10 @@
 open-dobot driver.
 
 Implements driver to open firmware that controls Dobot FPGA.
-Abstracts communication protocol, CCITT CRC and commands sent to FPGA.
+Abstracts communication protocol, CCITT CRC, and commands sent to FPGA.
 Find firmware and SDK at https://github.com/maxosprojects/open-dobot
 
-Author: maxosprojects (March 18 2016)
+Author: maxosprojects (March 18, 2016)
 Additional Authors: <put your name here>
 
 Version: 1.2.2
@@ -57,7 +57,7 @@ class DobotDriver:
 
 	def Open(self, timeout=0.025):
 		try:
-			self._port = serial_aggregator(serial.Serial(self._comport, baudrate=self._rate, timeout=timeout, interCharTimeout=0.1))
+			self._port = SerialAggregator(serial.Serial(self._comport, baudrate=self._rate, timeout=timeout, interCharTimeout=0.1))
 			# self._port = serial.Serial(self._comport, baudrate=self._rate, timeout=timeout, interCharTimeout=0.1)
 
 			# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -112,35 +112,35 @@ class DobotDriver:
 
 	def _readchecksumword(self):
 		data = self._port.read(2)
-		if len(data)==2:
+		if len(data) == 2:
 			arr = bytearray(data)
-			crc = (arr[0]<<8) | arr[1]
-			return (1,crc)
-		return (0,0)
+			crc = (arr[0] << 8) | arr[1]
+			return 1, crc
+		return 0, 0
 
 	def _readbyte(self):
 		data = self._port.read(1)
 		if len(data):
 			val = bytearray(data)[0]
 			self._crc_update(val)
-			return (1,val)	
-		return (0,0)
+			return 1, val
+		return 0, 0
 
 	def _readword(self):
 		val1 = self._readbyte()
 		if val1[0]:
 			val2 = self._readbyte()
 			if val2[0]:
-				return (1,val1[1]<<8|val2[1])
-		return (0,0)
+				return 1, val1[1] << 8 | val2[1]
+		return 0, 0
 
 	def _readsword(self):
 		val = self._readword()
 		if val[0]:
-			if val[1]&0x8000:
-				return (val[0],val[1]-0x10000)
-			return (val[0],val[1])
-		return (0,0)
+			if val[1] & 0x8000:
+				return val[0], val[1] - 0x10000
+			return val[0], val[1]
+		return 0, 0
 
 	def _readlong(self):
 		val1 = self._readbyte()
@@ -151,16 +151,16 @@ class DobotDriver:
 				if val3[0]:
 					val4 = self._readbyte()
 					if val4[0]:
-						return (1,val1[1]<<24|val2[1]<<16|val3[1]<<8|val4[1])
-		return (0,0)	
+						return 1, val1[1] << 24 | val2[1] << 16 | val3[1] << 8 | val4[1]
+		return 0, 0
 
 	def _readslong(self):
 		val = self._readlong()
 		if val[0]:
-			if val[1]&0x80000000:
-				return (val[0],val[1]-0x100000000)
-			return (val[0],val[1])
-		return (0,0)
+			if val[1] & 0x80000000:
+				return val[0], val[1] - 0x100000000
+			return val[0], val[1]
+		return 0, 0
 
 	def _read1(self, cmd):
 		return self._read(cmd, [self._readbyte])
@@ -193,7 +193,9 @@ class DobotDriver:
 								self._readslong,
 								self._readslong])
 
-	def _read(self, cmd, read_commands=list()):
+	def _read(self, cmd, read_commands=None):
+		if read_commands is None:
+			read_commands = []
 		trys = _max_trys
 		while trys:
 			self._sendcommand(cmd)
@@ -209,34 +211,36 @@ class DobotDriver:
 
 			crc = self._readchecksumword()
 			if crc[0]:
-				if self._crc&0xFFFF == crc[1]&0xFFFF:
+				if self._crc & 0xFFFF == crc[1] & 0xFFFF:
 					return tuple(ret)
 			trys -= 1
 		return tuple([0] * (len(read_commands) + 1))
 
 	def _writebyte(self, val):
-		self._crc_update(val&0xFF)
-		self._port.write(bytearray([val&0xFF]))
+		self._crc_update(val & 0xFF)
+		self._port.write(bytearray([val & 0xFF]))
 
 	def _writeword(self, val):
-		self._writebyte((val>>8)&0xFF)
-		self._writebyte(val&0xFF)
+		self._writebyte((val >> 8) & 0xFF)
+		self._writebyte(val & 0xFF)
 
 	def _writelong(self, val):
-		self._writebyte((val>>24)&0xFF)
-		self._writebyte((val>>16)&0xFF)
-		self._writebyte((val>>8)&0xFF)
-		self._writebyte(val&0xFF)
+		self._writebyte((val >> 24) & 0xFF)
+		self._writebyte((val >> 16) & 0xFF)
+		self._writebyte((val >> 8) & 0xFF)
+		self._writebyte(val & 0xFF)
 
 	def _writechecksum(self):
-		self._port.write(bytearray([(self._crc>>8)&0xFF]))
-		self._port.write(bytearray([self._crc&0xFF]))
+		self._port.write(bytearray([(self._crc >> 8) & 0xFF]))
+		self._port.write(bytearray([self._crc & 0xFF]))
 
 	def _sendcommand(self, command):
 		self._crc_clear()
 		self._writebyte(command)
 
-	def _write(self, cmd, write_commands=list()):
+	def _write(self, cmd, write_commands=None):
+		if write_commands is None:
+			write_commands = []
 		trys = _max_trys
 		while trys:
 			self._sendcommand(cmd)
@@ -248,7 +252,7 @@ class DobotDriver:
 			self._port.send()
 			crc = self._readchecksumword()
 			if crc[0]:
-				if self._crc&0xFFFF == crc[1]&0xFFFF:
+				if self._crc & 0xFFFF == crc[1] & 0xFFFF:
 					return True
 			trys -= 1
 		return False
@@ -337,31 +341,31 @@ class DobotDriver:
 		return ((val & 0x00FF) << 8) | ((val & 0xFF00) >> 8)
 
 	def freqToCmdVal(self, freq):
-		'''
+		"""
 		Converts stepping frequency into a command value that dobot takes.
-		'''
+		"""
 		if freq == 0:
 			return self._stopSeq
-		return self.reverseBits32(long((self._freqCoeff) / freq))
+		return self.reverseBits32(long(self._freqCoeff / freq))
 
 	def stepsToCmdVal(self, steps):
-		'''
-		Converts number of steps for dobot to do in 20ms into a command value that dobot
+		"""
+		Converts the number of steps for dobot to do in 20ms into a command value that dobot
 		takes to set the stepping frequency.
-		'''
+		"""
 		if steps == 0:
 			return self._stopSeq
 		return self.reverseBits32(long(self._stepCoeff / steps))
 
 	def stepsToCmdValFloat(self, steps):
-		'''
-		Converts number of steps for dobot to do in 20ms into a command value that dobot
+		"""
+		Converts the number of steps for dobot to do in 20ms into a command value that dobot
 		takes to set the stepping frequency.
 
 		@param steps - float number of steps; float to minimize error and have finer control
 		@return tuple (command_value, leftover), where leftover is the fractioned steps that don't fit
-				into 20ms interval a command runs for
-		'''
+				into the 20ms interval a command runs for
+		"""
 		if abs(steps) < 0.01:
 			return (self._stopSeq, 0, 0.0)
 		# "round" makes leftover negative in certain cases and causes backlash compensation to oscillate.
@@ -381,13 +385,15 @@ class DobotDriver:
 	def accel3DXToAngle(self, x, y, z):
 		return self.accel3DXToRadians(x, y, z) * piToDegrees
 
-	def accelToRadians(self, val, offset):
+	@staticmethod
+	def accelToRadians(val, offset):
 		try:
 			return math.asin(float(val - offset) / 493.56)
 		except ValueError:
 			return halfPi
 
-	def accel3DXToRadians(self, x, y, z):
+	@staticmethod
+	def accel3DXToRadians(x, y, z):
 		try:
 			xf = float(x)
 			yf = float(y)
@@ -397,11 +403,11 @@ class DobotDriver:
 			return halfPi
 
 	def CalibrateJoint(self, joint, forwardCommand, backwardCommand, direction, pin, pinMode, pullup):
-		'''
+		"""
 		Initiates joint calibration procedure using a limit switch/photointerrupter. Effective immediately.
-		Current command buffer is cleared.
+		The current command buffer is cleared.
 		Cancel the procedure by issuing EmergencyStop() is necessary.
-		
+	
 		@param joint - which joint to calibrate: 1-3
 		@param forwardCommand - command to send to the joint when moving forward (towards limit switch);
 				use freqToCmdVal()
@@ -413,7 +419,7 @@ class DobotDriver:
 		@param pinMode - limit switch/photointerrupter normal LOW = 0, normal HIGH = 1
 		@param pullup - enable pullup on the pin = 1, disable = 0
 		@return True if command succesfully received, False otherwise.
-		'''
+		"""
 		if 1 > joint > 3:
 			return False
 		control = ((pinMode & 0x01) << 4) | ((pullup & 0x01) << 3) | ((direction & 0x01) << 2) | ((joint - 1) & 0x03)
@@ -423,18 +429,18 @@ class DobotDriver:
 		return result
 
 	def EmergencyStop(self):
-		'''
+		"""
 		Stops the arm in case of emergency. Clears command buffer and cancels calibration procedure.
 
-		@return True if command succesfully received, False otherwise.
-		'''
+		@return True if the command successfully received, False otherwise.
+		"""
 		self._lock.acquire()
 		result = self._write0(CMD_EMERGENCY_STOP)
 		self._lock.release()
 		return result
 
 	def Steps(self, j1, j2, j3, j1dir, j2dir, j3dir, servoGrab, servoRot, deferred=False):
-		'''
+		"""
 		Adds a command to the controller's queue to execute on FPGA.
 		@param j1 - joint1 subcommand
 		@param j2 - joint2 subcommand
@@ -449,8 +455,8 @@ class DobotDriver:
 		@return Returns a tuple where the first element tells whether the command has been successfully
 		received (1 - received, 0 - timed out), and the second element tells whether the command was added
 		to the controller's command queue (1 - added, 0 - not added, as the queue was full).
-		'''
-		control = (j1dir & 0x01) | ((j2dir & 0x01) << 1) | ((j3dir & 0x01) << 2);
+		"""
+		control = (j1dir & 0x01) | ((j2dir & 0x01) << 1) | ((j3dir & 0x01) << 2)
 		# if deferred:
 		# 	control |= 0x01
 		self._lock.acquire()
@@ -464,10 +470,10 @@ class DobotDriver:
 			servoRot = 0
 
 		if self._ramps:
-			servoRot *= 2;
-			servoRot += 2000;
-			servoGrab *= 2;
-			servoGrab += 2000;
+			servoRot *= 2
+			servoRot += 2000
+			servoGrab *= 2
+			servoGrab += 2000
 
 		self._toolRotation = servoRot
 		self._gripper = servoGrab
@@ -477,9 +483,9 @@ class DobotDriver:
 		return result
 
 	def ExecQueue(self):
-		'''
+		"""
 		Executes deferred commands.
-		'''
+		"""
 		raise NotImplementedError()
 		self._lock.acquire()
 		result = self._write0(CMD_EXEC_QUEUE)
@@ -487,46 +493,42 @@ class DobotDriver:
 		return result
 
 	def GetAccelerometers(self):
-		'''
-		Returns data aquired from accelerometers at power on.
-		There are 17 reads in FPGA version and 20 reads in RAMPS version of each accelerometer
+		"""
+		Returns data acquired from accelerometers at power on.
+		There are 17 reads in an FPGA version and 20 reads in the RAMPS version of each accelerometer
 		that the firmware does and then averages the result before returning it here.
-		'''
+		"""
 		self._lock.acquire()
 		result = self._reads222222(CMD_GET_ACCELS)
 		self._lock.release()
 		return result
 
 	def GetCounters(self):
-		'''
-		'''
 		self._lock.acquire()
 		result = self._reads444(CMD_GET_COUNTERS)
 		self._lock.release()
 		return result
 
 	def SetCounters(self, base, rear, fore):
-		'''
-		'''
 		self._lock.acquire()
 		result = self._write444(CMD_SET_COUNTERS, base, rear, fore)
 		self._lock.release()
 		return result
 
 	def SwitchToAccelerometerReportMode(self):
-		'''
-		Apparently the following won't work because of the way dobot was desgined
+		"""
+		Apparently the following won't work because of the way dobot was designed
 		and limitations of AVR - cannot switch SPI from Slave to Master back.
-		So, as a workaround, just hold the "Sensor Calibration" button and start your
-		app. Arduino is reset on serial port connection and it takes about 2 seconds
+		So, as a workaround, hold the "Sensor Calibration" button and start your
+		app. Arduino is reset on serial port connection, and it takes about 2 seconds
 		for it to start. After that you can release the button. That switches dobot to
-		accelerometer reporting mode. To move the arm turn off the power switch.
+		accelerometer reporting mode. To move the arm, turn off the power switch.
 
 		This function is left just in case a proper fix comes up.
 
 		Switches dobot to accelerometer report mode.
 		Dobot must be reset to enter normal mode after issuing this command.
-		'''
+		"""
 		raise NotImplementedError('Read function description for more info')
 		self._lock.acquire()
 		result = self._write_read(CMD_SWITCH_TO_ACCEL_REPORT_MODE, [])
@@ -534,11 +536,11 @@ class DobotDriver:
 		return result
 
 	def LaserOn(self, on):
-		'''
+		"""
 		@return Returns a tuple where the first element tells whether the command has been successfully
 		received (0 - yes, 1 - timed out), and the second element tells whether the command was added
 		to the controller's command queue (1 - added, 0 - not added, as the queue was full).
-		'''
+		"""
 		self._lock.acquire()
 		if on:
 			result = self._write1read1(CMD_LASER_ON, 1)
@@ -548,14 +550,13 @@ class DobotDriver:
 		return result
 	
 	def PumpOn(self, on):
-		'''
-		Turn On/Off the pump motor, if you want actually grip something you also need to turn the valve
-		on otherwise the air just flow throught it.
-		
+		"""
+		Turn On/Off the pump motor, if you want to actually grip something, you also need to turn the valve on otherwise the air flows through it.
+	
 		@return Returns a tuple where the first element tells whether the command has been successfully
 		received (0 - yes, 1 - timed out), and the second element tells whether the command was added
 		to the controller's command queue (1 - added, 0 - not added, as the queue was full).
-		'''
+		"""
 		self._lock.acquire()
 		if on:
 			result = self._write1read1(CMD_PUMP_ON, 1)
@@ -565,14 +566,14 @@ class DobotDriver:
 		return result
 	
 	def ValveOn(self, on):
-		'''
-		Does not do much by itself but work in tandem with the pump, when the pump is on turning the valve
+		"""
+		Does little by itself but work in tandem with the pump, when the pump is on turning the valve
 		on will allow you to grab things, turn it off to release the air pressure.
-		
+	
 		@return Returns a tuple where the first element tells whether the command has been successfully
 		received (0 - yes, 1 - timed out), and the second element tells whether the command was added
 		to the controller's command queue (1 - added, 0 - not added, as the queue was full).
-		'''
+		"""
 		self._lock.acquire()
 		if on:
 			result = self._write1read1(CMD_VALVE_ON, 1)
@@ -582,14 +583,13 @@ class DobotDriver:
 		return result
 
 	def Wait(self, waitTime):
-		'''
-		Makes the arm wait in current position for the specified period of time. The wait period is specified
-		in seconds and can be fractions of seconds.
+		"""
+		Makes the arm wait in the current position for the specified period of time. The wait period is specified in seconds and can be fractions of seconds.
 		The resolution of this command is up to 20ms.
 
-		In order to make the arm wait a number of commands is issued to do nothing. Each command takes 20ms
+		To make the arm wait, a number of commands is issued to do nothing. Each command takes 20ms
 		to execute by the arm.
-		'''
+		"""
 		iterations = int(waitTime * 50)
 		for i in range(iterations):
 			ret = (0, 0)
@@ -598,10 +598,10 @@ class DobotDriver:
 				ret = self.Steps(0, 0, 0, 0, 0, 0, self._gripper, self._toolRotation)
 
 	def BoardVersion(self):
-		'''
+		"""
 		Checks board version.
 		Returns (success, version), where version=0 is FPGA and version=1 is RAMPS.
-		'''
+		"""
 		self._lock.acquire()
 		result = self._read1(CMD_BOARD_VERSION)
 		self._lock.release()
@@ -614,13 +614,13 @@ class DobotDriver:
 		return self._ramps == self.RAMPS
 
 	def isReady(self):
-		'''
+		"""
 		Checks whether the controller is up and running.
-		'''
+		"""
 		self._lock.acquire()
 		result = self._read1(CMD_READY)
 		self._lock.release()
-		# Check for magic number.
+		# Check for the magic number.
 		# return [result[0], result[1] == 0x40]
 		return result
 
@@ -640,7 +640,7 @@ This is a workaround to send data in bursts on systems that have slow API
 used by pyserial (e.g. Windows).
 
 """
-class serial_aggregator:
+class SerialAggregator:
 	def __init__(self, ser):
 		self._ser = ser
 		self._buf = bytearray()
