55d39838367fea37c4e8f0c13f9a80b4a8134b4e
Ingo Breßler <dev@ingobressler.net>
2026-04-02 10:56:23 +0200

    refactor(DobotSDK): clean up unused methods, improve formatting, and fix minor issues

--- dobot/DobotSDK.py
+++ dobot/DobotSDK.py
@@ -25,15 +25,10 @@ Version: 1.2.2
 License: MIT
 """
 
-import serial
-import threading
-import time
-from serial import SerialException
-from dobot.DobotDriver import DobotDriver
-from dobot.DobotKinematics import *
-import timeit
-import math
 import sys
+import math
+from dobot.DobotDriver import DobotDriver
+from dobot.DobotKinematics import DobotKinematics, piHalf, piTwo
 
 # Workaround to support Python 2/3
 if sys.version_info > (3,):
@@ -58,9 +53,15 @@ frontArmMicrosteppingMultiplier = 16.0
 stepperPlanetaryGearBoxMultiplier = 10.0
 
 # calculate the actual number of steps it takes for each stepper motor to rotate 360 degrees
-baseActualStepsPerRevolution = stepperMotorStepsPerRevolution * baseMicrosteppingMultiplier * stepperPlanetaryGearBoxMultiplier
-rearArmActualStepsPerRevolution = stepperMotorStepsPerRevolution * rearArmMicrosteppingMultiplier * stepperPlanetaryGearBoxMultiplier
-frontArmActualStepsPerRevolution = stepperMotorStepsPerRevolution * frontArmMicrosteppingMultiplier * stepperPlanetaryGearBoxMultiplier
+baseActualStepsPerRevolution = (
+    stepperMotorStepsPerRevolution * baseMicrosteppingMultiplier * stepperPlanetaryGearBoxMultiplier
+)
+rearArmActualStepsPerRevolution = (
+    stepperMotorStepsPerRevolution * rearArmMicrosteppingMultiplier * stepperPlanetaryGearBoxMultiplier
+)
+frontArmActualStepsPerRevolution = (
+    stepperMotorStepsPerRevolution * frontArmMicrosteppingMultiplier * stepperPlanetaryGearBoxMultiplier
+)
 
 class Dobot:
     pos = None
@@ -120,8 +121,8 @@ class Dobot:
             frontAngle = self._driver.accelToRadians(accelFrontX, accelOffsets[1])
         else:
             # In RAMPS accelerometers are on I2C bus and can be read at any time. We need to
-            # read them multiple times to get average as MPU-6050 have greater resolution but are noisy.
-            # However, due to the interference from the way A4988 holds the motors if none of the
+            # read them multiple times to get average as MPU-6050 has greater resolution but is noisy.
+            # However, due to the interference from the way A4988 holds the motors, if none of the
             # recommended measures to suppress interference are in place (see open-dobot wiki), or
             # in case accelerometers are not connected, we need to give up and assume some predefined pose.
             accelRearX = 0
@@ -131,7 +132,7 @@ class Dobot:
             accelFrontY = 0
             accelFrontZ = 0
             successes = 0
-            for i in range(20):
+            for _ in range(20):
                 ret = (0, 0, 0, 0, 0, 0, 0)
                 attempts = 10
                 while attempts:
@@ -148,10 +149,15 @@ class Dobot:
                     attempts -= 1
             if successes > 0:
                 divisor = float(successes)
-                rearAngle = piHalf - self._driver.accel3DXToRadians(accelRearX / divisor, accelRearY / divisor, accelRearZ / divisor)
-                frontAngle = -self._driver.accel3DXToRadians(accelFrontX / divisor, accelFrontY / divisor, accelFrontZ / divisor)
+                rearAngle = piHalf - self._driver.accel3DXToRadians(
+                    accelRearX / divisor, accelRearY / divisor, accelRearZ / divisor
+                )
+                frontAngle = -self._driver.accel3DXToRadians(
+                    accelFrontX / divisor, accelFrontY / divisor, accelFrontZ / divisor
+                )
             else:
-                print('Failed to read accelerometers. Make sure they are connected and interference is suppressed. See open-dobot wiki')
+                print('Failed to read accelerometers. Make sure they are connected and interference is suppressed.')
+                print('See open-dobot wiki')
                 print('Assuming rear arm vertical and front arm horizontal')
                 rearAngle = 0
                 frontAngle = -piHalf
@@ -168,58 +174,8 @@ class Dobot:
         print('Current estimated coordinates:', self.pos)
         print("--=========--")
 
-    def _moveArmToAngles(self, baseAngle, rearArmAngle, frontArmAngle, duration):
-        self._baseAngle = baseAngle
-        self._rearAngle = rearArmAngle
-        self._frontAngle = frontArmAngle
-        dur = float(duration)
-
-        # baseStepLocation = long((baseAngle / 360.0) * baseActualStepsPerRevolution + 0.5)
-        # rearArmStepLocation = long((abs(rearArmAngle) / 360.0) * rearArmActualStepsPerRevolution + 0.5)
-        # frontArmStepLocation = long((abs(frontArmAngle) / 360.0) * frontArmActualStepsPerRevolution + 0.5)
-        baseStepLocation = long(baseAngle * baseActualStepsPerRevolution / piTwo)
-        rearArmStepLocation = long(rearArmAngle * rearArmActualStepsPerRevolution / piTwo)
-        frontArmStepLocation = long(frontArmAngle * frontArmActualStepsPerRevolution / piTwo)
-
-        self._debug("Base Step Location", baseStepLocation)
-        self._debug("Rear Arm Step Location", rearArmStepLocation)
-        self._debug("Frontarm Step Location", frontArmStepLocation)
-
-        baseDiff = baseStepLocation - self._baseSteps
-        rearDiff = rearArmStepLocation - self._rearSteps
-        frontDiff = frontArmStepLocation - self._frontSteps
-        self._debug('baseDiff', baseDiff)
-        self._debug('rearDiff', rearDiff)
-        self._debug('frontDiff', frontDiff)
-
-        self._baseSteps = baseStepLocation
-        self._rearSteps = rearArmStepLocation
-        self._frontSteps = frontArmStepLocation
-
-        baseDir = 1
-        rearDir = 1
-        frontDir = 1
-
-        if (baseDiff < 1):
-            baseDir = 0
-        if (rearDiff < 1):
-            rearDir = 0
-        if (frontDiff > 1):
-            frontDir = 0
-
-        baseSliced = self._sliceStepsToValues(abs(baseDiff), dur)
-        rearSliced = self._sliceStepsToValues(abs(rearDiff), dur)
-        frontSliced = self._sliceStepsToValues(abs(frontDiff), dur)
-
-        for base, rear, front in zip(baseSliced, rearSliced, frontSliced):
-            ret = [0, 0]
-            # If ret[0] == 0 then command timed out or crc failed.
-            # If ret[1] == 0 then command queue was full.
-            while ret[0] == 0 or ret[1] == 0:
-                ret = self._driver.Steps(base, rear, front, baseDir, rearDir, frontDir)
 
     def _moveToAnglesSlice(self, baseAngle, rearArmAngle, frontArmAngle, toolRotation):
-
         baseStepLocation = baseAngle * baseActualStepsPerRevolution / piTwo
         rearArmStepLocation = abs(rearArmAngle * rearArmActualStepsPerRevolution / piTwo)
         frontArmStepLocation = abs(frontArmAngle * frontArmActualStepsPerRevolution / piTwo)
@@ -247,13 +203,13 @@ class Dobot:
         rearDir = 1
         frontDir = 1
 
-        if (baseDiff < 1):
+        if baseDiff < 1:
             baseDir = 0
             baseSign = -1
-        if (rearDiff < 1):
+        if rearDiff < 1:
             rearDir = 0
             rearSign = -1
-        if (frontDiff > 1):
+        if frontDiff > 1:
             frontDir = 0
             frontSign = 1
 
@@ -282,82 +238,30 @@ class Dobot:
             # Repeat until the command is queued. May not be queued if queue is full.
             ret = (0, 0)
             while not ret[1]:
-                ret = self._driver.Steps(cmdBaseVal, cmdRearVal, cmdFrontVal, baseDir, rearDir, frontDir, self._gripper, int(toolRotation))
+                ret = self._driver.Steps(
+                    cmdBaseVal, cmdRearVal, cmdFrontVal, baseDir, rearDir, frontDir, self._gripper, int(toolRotation)
+                )
 
         if self._plot:
             self._toPlot1.append(baseDiff)
             self._toPlot2.append(actualStepsBase * baseSign)
 
-        return (actualStepsBase * baseSign, actualStepsRear * rearSign, actualStepsFront * frontSign,\
-                    leftStepsBase * baseSign, leftStepsRear * rearSign, leftStepsFront * frontSign)
+        return (
+            actualStepsBase * baseSign, actualStepsRear * rearSign, actualStepsFront * frontSign,
+            leftStepsBase * baseSign, leftStepsRear * rearSign, leftStepsFront * frontSign
+        )
 
     def freqToCmdVal(self, freq):
-        '''
+        """
         See DobotDriver.freqToCmdVal()
-        '''
+        """
         return self._driver.freqToCmdVal(freq)
 
-    def _moveTo(self, x, y, z, duration):
-        '''
-        TEMPORARILY UNAVALAILABLE
-        Left for future refactor.
-
-        Moves the arm to the location with provided coordinates. Makes sure it takes exactly
-        the amount of time provided in "duration" argument to get end effector to the location.
-        All axes arrive at the same time.
-        SDK keeps track of the current end effector pose.
-        The origin is at the arm's base (the rotating base plate - joint1).
-        Refer to docs/images/arm-description.png and docs/images/reference-frame.png to find
-        more about reference frame and arm names.
-        Current limitations:
-        - rear arm cannot go beyond initial 90 degrees (backwards from initial vertical position),
-          so plan carefully the coordinates
-        - front arm cannot go above initial horizontal position
-        Not yet implemented:
-        - the move is not linear as motors run the whole path at constant speed
-        - acceleration/deceleration
-        - rear arm cannot go beyond 90 degrees (see current limitations) and there are no checks
-          for that - it will simply go opposite direction instead
-        '''
-        xx = float(x)
-        yy = float(y)
-        zz = float(z)
-
-        radiansToDegrees = 180.0 / math.pi
-
-        b = 135.0
-        c = 160.0
-        b2 = pow(b, 2)
-        c2 = pow(c, 2)
-
-        self._debug('============================')
-
-        r = math.sqrt(pow(xx, 2) + pow(yy, 2))
-        self._debug('r', r)
-        z2 = zz - (80.0 + 23.0)
-        self._debug('z2', z2)
-        d2 = math.pow(z2, 2) + math.pow(r, 2)
-        d = math.sqrt(d2)
-        self._debug('d', d)
-        self._debug('d2', d2)
-
-        q1 = math.atan2(z2, r)
-        self._debug('q1', q1, q1 * radiansToDegrees)
-        q2 = math.acos((b2 - c2 + d2) / (2.0 * b * d))
-        self._debug('q2', q2, q2 * radiansToDegrees)
-        rearAngle = piHalf - (q1 + q2)
-        self._debug('ik rear angle', rearAngle, rearAngle * radiansToDegrees)
-        frontAngle = piHalf - (math.acos((b2 + c2 - d2) / (2.0 * b * c)) - rearAngle)
-        self._debug('ik front angle', frontAngle, frontAngle * radiansToDegrees)
-        baseAngle = math.atan2(yy, xx)
-        self._debug('ik base angle', baseAngle, baseAngle * radiansToDegrees)
-
-        self._moveArmToAngles(baseAngle, rearAngle, frontAngle, duration)
 
     def MoveWithSpeed(self, x, y, z, maxSpeed, accel=None, toolRotation=None):
-        '''
+        """
         For toolRotation see DobotDriver.Steps() function description (servoRot parameter).
-        '''
+        """
 
         if self._plot:
             self._toPlot1 = []
@@ -368,7 +272,7 @@ class Dobot:
         yy = float(y)
         zz = float(z)
 
-        if toolRotation == None:
+        if toolRotation is None:
             toolRotation = self._toolRotation
         elif toolRotation > 1024:
             toolRotation = 1024
@@ -377,7 +281,7 @@ class Dobot:
 
         accelf = None
         # Set 100% acceleration to equal maximum velocity if it wasn't provided
-        if accel == None:
+        if accel is None:
             accelf = maxVel
         else:
             accelf = float(accel)
@@ -413,7 +317,7 @@ class Dobot:
             timeFlat = 0
             flatSlices = 0
             maxVel = math.sqrt(distance * accelf)
-        # Or else number of slices when velocity does not change is greater than zero.
+        # Or else the number of slices when velocity does not change is greater than zero.
         else:
             timeToAccel = maxVel / accelf
             accelSlices = timeToAccel * 50.0
@@ -457,15 +361,8 @@ class Dobot:
         leftStepsRear = 0.0
         leftStepsFront = 0.0
         if self._plot:
-            toPlot1 = []
-            toPlot2 = []
-            toPlot3 = []
-            toPlot4 = []
-            toPlot5 = []
-            toPlot6 = []
-            toPlot7 = []
-            toPlot8 = []
-            toPlot9 = []
+            toPlot1, toPlot2, toPlot3, toPlot4, toPlot5, toPlot6, toPlot7, toPlot8, toPlot9 = \
+                ([], [], [], [], [], [], [], [], [])
         while commands < slices:
             self._debug('==============================')
             self._debug('slice #', commands)
@@ -492,7 +389,7 @@ class Dobot:
             nextToolRotation = self._toolRotation + (segmentToolRotation * commands)
             self._debug('nextToolRotation', nextToolRotation)
 
-            (baseAngle, rearAngle, frontAngle) = self._kinematics.anglesFromCoordinates(nextX, nextY, nextZ)
+            baseAngle, rearAngle, frontAngle = self._kinematics.anglesFromCoordinates(nextX, nextY, nextZ)
 
             movedStepsBase, movedStepsRear, movedStepsFront, leftStepsBase, leftStepsRear, leftStepsFront = \
                 self._moveToAnglesSlice(baseAngle, rearAngle, frontAngle, nextToolRotation)
@@ -545,39 +442,38 @@ class Dobot:
             self._plt.plot(self._toPlot2, linewidth=2.0)
             # make the y ticks integers, not floats
             yint = []
-            locs, labels = self._plt.yticks()
+            locs, _ = self._plt.yticks()
             for each in locs:
                 yint.append(int(each))
             self._plt.yticks(yint)
             self._plt.show()
 
-    def Gripper(self, gripper):
-        if gripper > 480:
+    def Gripper(self, value):
+        if value > 480:
             self._gripper = 480
-        elif gripper < 208:
+        elif value < 208:
             self._gripper = 208
         else:
-            self._gripper = gripper
+            self._gripper = value
 
         self._driver.Steps(0, 0, 0, 0, 0, 0, self._gripper, self._toolRotation)
 
     def Wait(self, waitTime):
-        '''
+        """
         See description in DobotDriver.Wait()
-        '''
+        """
         self._driver.Wait(waitTime)
 
     def CalibrateJoint(self, joint, forwardCommand, backwardCommand, direction, pin, pinMode, pullup):
-        '''
+        """
         See DobotDriver.CalibrateJoint()
-        '''
+        """
         return self._driver.CalibrateJoint(joint, forwardCommand, backwardCommand, direction, pin, pinMode, pullup)
 
     def EmergencyStop(self):
-        '''
+        """
         See DobotDriver.EmergencyStop()
-        '''
-
+        """
         return self._driver.EmergencyStop()
 
     def LaserOn(self, on):
